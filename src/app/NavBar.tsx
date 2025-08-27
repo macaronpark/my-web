c81119d3c7701ee361497358bdf64c07bfa46811
macaronpark <pxxxsxzy.0@gmail.com>
2025-08-27 17:06:55 +0900

    feat: tailwind 유틸리티 클래스 오타 수정

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar({ children }: Props) {
   return (
     <nav
       className={`
-        flex flex-wrap flex-start gap-0.5 w-full px-1 py-0.5 
+        flex flex-wrap justify-start gap-0.5 w-full px-1 py-0.5 
         bg-win95-lightgray bevel-raised border-l-0 border-r-0
       `}
     >
